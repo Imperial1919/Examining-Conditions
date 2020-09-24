6806e53f0497699fe2aea773dcf70b192bcf3dc0
Bryce <ImperialEagle1998@gmail.com>
2020-09-23 22:36:38 -0500

    A early Draft of a basic lesson in examing conditions.

---
+++ TernaryOperatorPractice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TernaryOperatorPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+
+             a = 25;
+            b = 30;
+
+            int test = (a > b) ? a : b;
+            Console.WriteLine("Ok, s the greatest number here is: \n  " + test);
+            string test2 = (a == 10) ? "Hello":"Goodbye";
+            Console.WriteLine(test2);
+
+
+        }
+    }
+}
