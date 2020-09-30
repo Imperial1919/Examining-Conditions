e1576e04579cfc0d4e36e81a4d8f9d4c53aea51a
Bryce <ImperialEagle1998@gmail.com>
2020-09-30 16:33:13 -0500

    Added a quick user input module.

--- TernaryOperatorPractice/Program.cs
+++ TernaryOperatorPractice/Program.cs
@@ -17,6 +17,38 @@ namespace TernaryOperatorPractice
             string test2 = (a == 10) ? "Hello":"Goodbye";
             Console.WriteLine(test2);
 
+            bool locomotivesareavalible = true ;
+          
+
+            var steamer = (locomotivesareavalible == true) ? "Yes, we have a locomotive in stock" : "No, we do not have a locomotive in stock";
+            Console.WriteLine(steamer);
+            var outofstock = (locomotivesareavalible != true) ? "We do not have a locomotive in stock" : "eh, sure, why not?";
+            Console.WriteLine(outofstock);
+
+
+            bool carsareavalible = true;
+            bool classiccarsinstock = true;
+
+            var firstsale = (carsareavalible == true) ? "Ok, so We do have some cars out back, cannot assure you in regards to classic cars, but \n We DO have some cars open for purchase." : "Sorry Sir/Ma'am, At this present moment we lack anything in stock, please return later.";
+            Console.WriteLine("Welcome to Bryce' Automotive Emperorium, where We upkeep and maintain exotic cars from all eras, past or new. If you are interested in buying anything from us, please type in y or n as single characters please.");
+            string userinput = Console.ReadLine();
+
+            if (userinput == "y")
+            {
+                Console.WriteLine(firstsale);
+            }
+
+            else if  (userinput == "n")
+            {
+                Console.WriteLine("Ok then, have a nice day and thanks for wasting Our time! :D");
+            }
+
+            else
+            {
+                Console.WriteLine("... What?.. huh?... Just.. I'm calling the police Sir/Ma'am, You aren't well.");
+            }
+           
+
 
         }
     }
